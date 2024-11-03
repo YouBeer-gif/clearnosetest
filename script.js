a67ae390b070d221ad38868744b7fe1d9c45b2eb
YouBeer-gif <phichratnmisrrphwngs@gmail.com>
2024-11-03 14:57:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-window.onload = function() {
-    // ดึงค่าภาษาจาก localStorage
-    let language = localStorage.getItem('selectedLanguage');
-
-    Chooselanguage(language);
-};
-
-function Chooselanguage(language) {
-
-    localStorage.setItem('selectedLanguage', language);
-    
-    let change = {
-        TH: {
-            PRODUCTS: 'สินค้า',
-            STORELOCATOR: 'ที่ตั้งร้านค้า',
-            SOCIETY: 'สังคม',
-            CONTACTUS: 'ติดต่อเรา',
+function Chooselanguage (language){
+    const choose = {
+        TH : {
+            products : 'สินค้า',
         },
-        EN: {
-            PRODUCTS: 'PRODUCTS',
-            STORELOCATOR: 'STORE LOCATOR',
-            SOCIETY: 'SOCIETY',
-            CONTACTUS: 'CONTACT US',
+        EN : {
+            products : 'Products',
         }
     };
-    
-    // อัปเดตเนื้อหาขององค์ประกอบใน DOM
-    document.getElementById('PRODUCTS').innerText = change[language].PRODUCTS;
-    document.getElementById('STORELOCATOR').innerText = change[language].STORELOCATOR;
-    document.getElementById('SOCIETY').innerText = change[language].SOCIETY;
-    document.getElementById('CONTACTUS').innerText = change[language].CONTACTUS;
+    document.getElementById('products').innerText = choose[language].products;
 }
